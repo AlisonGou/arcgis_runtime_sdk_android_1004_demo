d0785876d0779154ce5b2fba55edac327b005e68
AlisonGou <gougexueli@hotmail.com>
2018-12-26 15:04:56 +0800

    show two fragments of mapview in one activity

---
+++ mapviewfragments/src/main/java/com/example/gougxl/mapviewfragments/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.gougxl.mapviewfragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_relative);
+
+
+        FragmentManager fm=getSupportFragmentManager();
+        Fragment fragmentleft = fm.findFragmentById(R.id.fragment_container_left);
+        Fragment fragmentright= fm.findFragmentById(R.id.fragment_container_right);
+
+        if (fragmentleft==null){
+            fragmentleft=new MapViewFragment();
+            fm.beginTransaction().add(R.id.fragment_container_left,fragmentleft).commit();
+        }
+        if (fragmentright==null){
+            fragmentright=new MapViewFragment();
+            fm.beginTransaction().add(R.id.fragment_container_right,fragmentright).commit();
+        }
+    }
+}
---
+++ mapviewfragments/src/main/java/com/example/gougxl/mapviewfragments/MapViewFragment.java
@@ -0,0 +1,32 @@
+package com.example.gougxl.mapviewfragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
+public class MapViewFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragmentlayout,container,false);
+        MapView mapView = view.findViewById(R.id.mapview);
+        ArcGISMap arcGISMap = new ArcGISMap(Basemap.createOpenStreetMap());
+        mapView.setMap(arcGISMap);
+
+        return view;
+    }
+}
